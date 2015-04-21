9d2163f471e2d84c9501b0ea6689bde99406b9c1
Dmitry666 <gamexgroup@gmail.com>
2015-04-21 16:18:31 +0600

    Refactoring code standart.

--- mime_types.cpp
+++ mime_types.cpp
@@ -16,28 +16,28 @@ namespace mime_types {
 
 struct mapping
 {
-  const char* extension;
-  const char* mime_type;
+    const char* extension;
+    const char* mime_type;
 } mappings[] =
 {
-  { "gif", "image/gif" },
-  { "htm", "text/html" },
-  { "html", "text/html" },
-  { "jpg", "image/jpeg" },
-  { "png", "image/png" }
+    { "gif", "image/gif" },
+    { "htm", "text/html" },
+    { "html", "text/html" },
+    { "jpg", "image/jpeg" },
+    { "png", "image/png" }
 };
 
 std::string extension_to_type(const std::string& extension)
 {
-  for (mapping m: mappings)
-  {
-    if (m.extension == extension)
+    for (mapping m: mappings)
     {
-      return m.mime_type;
+        if (m.extension == extension)
+        {
+            return m.mime_type;
+        }
     }
-  }
 
-  return "text/plain";
+    return "text/plain";
 }
 
 } // namespace mime_types
