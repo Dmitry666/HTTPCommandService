066ea7134e57ecea699136ee25654d86c0abe01c
micron_eo <edicmor@unitsys.ru>
2021-06-30 18:27:12 +0700

    added uts remote

--- ThirdPartySetup.bat
+++ ThirdPartySetup.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 Set CONAN_REVISIONS_ENABLED=1
-set CONAN_DISABLE_CHECK_COMPILER
 
-conan install . -if=.\conan -s os=Windows -s arch=x86_64 -s build_type=Release -s compiler="Visual Studio" -s compiler.version=15
+conan remote add uts http://artifactory.unitsys.ru:8081/artifactory/api/conan/conan -f
+conan install . -if=.\conan -s os=Windows -s arch=x86_64 -s build_type=Release -s compiler="Visual Studio" -s compiler.version=15 -r=uts
 mkdir build & cd build
 cmake -G "Visual Studio 15 2017" -A x64 -S .. -B "x86_64"
 cmake --build "x86_64" --config Release
